37ee00627099be9ee7d49e6c005edaf50c2f158a
Jernice <jerniceduncan@icloud.com>
2020-04-05 04:53:21 -0700

    add files

---
+++ assets/js/moment.js
@@ -0,0 +1,182 @@
+(function(global, factory) {
+        typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+            typeof define === 'function' && define.amd ? define(factory) :
+            global.moment = factory()
+    }(this, (function() {
+        'use strict';
+
+        var hookCallback;
+
+        function hooks() {
+            return hookCallback.apply(null, arguments);
+        }
+
+        // This is done to register the method called with moment()
+        // without creating circular dependencies.
+        function setHookCallback(callback) {
+            hookCallback = callback;
+        }
+
+        function isArray(input) {
+            return input instanceof Array || Object.prototype.toString.call(input) === '[object Array]';
+        }
+
+        function isObject(input) {
+            // IE8 will treat undefined and null as object if it wasn't for
+            // input != null
+            return input != null && Object.prototype.toString.call(input) === '[object Object]';
+        }
+
+        function isObjectEmpty(obj) {
+            if (Object.getOwnPropertyNames) {
+                return (Object.getOwnPropertyNames(obj).length === 0);
+            } else {
+                var k;
+                for (k in obj) {
+                    if (obj.hasOwnProperty(k)) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+
+        function isUndefined(input) {
+            return input === void 0;
+        }
+
+        function isNumber(input) {
+            return typeof input === 'number' || Object.prototype.toString.call(input) === '[object Number]';
+        }
+
+        function isDate(input) {
+            return input instanceof Date || Object.prototype.toString.call(input) === '[object Date]';
+        }
+
+        function map(arr, fn) {
+            var res = [],
+                i;
+            for (i = 0; i < arr.length; ++i) {
+                res.push(fn(arr[i], i));
+            }
+            return res;
+        }
+
+        function hasOwnProp(a, b) {
+            return Object.prototype.hasOwnProperty.call(a, b);
+        }
+
+        function extend(a, b) {
+            for (var i in b) {
+                if (hasOwnProp(b, i)) {
+                    a[i] = b[i];
+                }
+            }
+
+            if (hasOwnProp(b, 'toString')) {
+                a.toString = b.toString;
+            }
+
+            if (hasOwnProp(b, 'valueOf')) {
+                a.valueOf = b.valueOf;
+            }
+
+            return a;
+        }
+
+        function createUTC(input, format, locale, strict) {
+            return createLocalOrUTC(input, format, locale, strict, true).utc();
+        }
+
+        function defaultParsingFlags() {
+            // We need to deep clone this object.
+            return {
+                empty: false,
+                unusedTokens: [],
+                unusedInput: [],
+                overflow: -2,
+                charsLeftOver: 0,
+                nullInput: false,
+                invalidMonth: null,
+                invalidFormat: false,
+                userInvalidated: false,
+                iso: false,
+                parsedDateParts: [],
+                meridiem: null,
+                rfc2822: false,
+                weekdayMismatch: false
+            };
+        }
+
+        function getParsingFlags(m) {
+            if (m._pf == null) {
+                m._pf = defaultParsingFlags();
+            }
+            return m._pf;
+        }
+
+        var some;
+        if (Array.prototype.some) {
+            some = Array.prototype.some;
+        } else {
+            some = function(fun) {
+                var t = Object(this);
+                var len = t.length >>> 0;
+
+                for (var i = 0; i < len; i++) {
+                    if (i in t && fun.call(this, t[i], i, t)) {
+                        return true;
+                    }
+                }
+
+                return false;
+            };
+        }
+
+        function isValid(m) {
+            if (m._isValid == null) {
+                var flags = getParsingFlags(m);
+                var parsedParts = some.call(flags.parsedDateParts, function(i) {
+                    return i != null;
+                });
+                var isNowValid = !isNaN(m._d.getTime()) &&
+                    flags.overflow < 0 &&
+                    !flags.empty &&
+                    !flags.invalidMonth &&
+                    !flags.invalidWeekday &&
+                    !flags.weekdayMismatch &&
+                    !flags.nullInput &&
+                    !flags.invalidFormat &&
+                    !flags.userInvalidated &&
+                    (!flags.meridiem || (flags.meridiem && parsedParts));
+
+                if (m._strict) {
+                    isNowValid = isNowValid &&
+                        flags.charsLeftOver === 0 &&
+                        flags.unusedTokens.length === 0 &&
+                        flags.bigHour === undefined;
+                }
+
+                if (Object.isFrozen == null || !Object.isFrozen(m)) {
+                    m._isValid = isNowValid;
+                } else {
+                    return isNowValid;
+                }
+            }
+            return m._isValid;
+        }
+
+        function createInvalid(flags) {
+            var m = createUTC(NaN);
+            if (flags != null) {
+                extend(getParsingFlags(m), flags);
+            } else {
+                getParsingFlags(m).userInvalidated = true;
+            }
+
+            return m;
+        };
+
+
+
+    }));
